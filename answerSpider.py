49a668881e5675e404fbaceac88723a74538fe8f
wyxplus <wyxplus@gmail.com>
2020-10-25 00:12:33 +0800

    更新代码

--- answerSpider.py
+++ answerSpider.py
@@ -6,6 +6,7 @@ import random
 import json
 import pymysql
 
+
 # 回答ID不唯一，经过测试发现在不同问题下会有相同回答ID情况出现
 
 # create table answer(
@@ -41,8 +42,7 @@ def answerSpider():
             db.commit()
         except pymysql.Error as e:
             print(e)
-            print(111)
-            print('操作数据库失败')
+            print('提取问题操作数据库失败')
         finally:
             if db.open:
                 db.close()
@@ -58,11 +58,21 @@ def answerSpider():
 
             for question in questionTuple:
                 url = 'https://www.zhihu.com/api/v4/questions/'+ str(question[0]) + '/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=20&offset=0&platform=desktop&sort_by=updated'
-                
+                proxy = requests.get("http://127.0.0.1:5010/get/", timeout=10).json().get("proxy")
+
                 # 爬取一个问题下的所有回答
                 while True:
-                    time.sleep(0.5)
-                    html = requests.get(url, headers=headers)
+                    while True:
+                        time.sleep(0.5)
+                        html = requests.get(url, headers=headers, proxies={"http": "http://{}".format(proxy)}, timeout=10)
+                        # html = requests.get(newURL, headers=headers)
+                        print(proxy + ' ' + str(html.status_code))
+                        if html.ok == True:
+                            break
+                        else:
+                            requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy), timeout=10)
+                            proxy = requests.get("http://127.0.0.1:5010/get/", timeout=10).json().get("proxy")
+
                     dataList = html.json()['data']
                     print(len(dataList))
                     for data in dataList:
@@ -75,9 +85,10 @@ def answerSpider():
                         sql_insert = "insert into answer(answer_id,question_id,upvoters_count,comment_count,created_time) value ({answer_id},{question_id},{upvoters_count},{comment_count},'{created_time}')".format(answer_id = answer_id, question_id = question_id, upvoters_count = upvoters_count, comment_count = comment_count, created_time = created_time)
                         cursor.execute(sql_insert)
                         db.commit()
-                    url = html.json()['paging']['next']
                     if html.json()['paging']['is_end'] == True:
                         break
+                    url = html.json()['paging']['next']
+                    
 
                 sql_update = "update question set get_answer=1 WHERE question_id={question_id}".format(question_id = question[0])
                 cursor.execute(sql_update)
@@ -85,11 +96,17 @@ def answerSpider():
                 print("question ID :" + str(question_id) + " is over")
         except pymysql.Error as e:
             print(e)
-            print(222222)
-            print('操作数据库失败')
+            print('插入回答操作数据库失败')
+            url = html.json()['paging']['next']
+        except Exception as e:
+            print(e)
+            requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy), timeout=10)
+            proxy = requests.get("http://127.0.0.1:5010/get/", timeout=10).json().get("proxy")
         finally:
             if db.open:
                 db.close()
     
 
 answerSpider()
+
+
